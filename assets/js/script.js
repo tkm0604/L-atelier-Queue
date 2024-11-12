e13d4327e75a6920cef9caa6b8e701e4396db7e4
yamaguchi.takumi <gu.jp06042gmail.com>
2024-11-12 16:21:49 +0900

    TOPへ戻るボタン実装

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 // -----------------------------------
 // デバイスで条件分岐
 // -----------------------------------
-$(function(){
-	var ua = window.navigator.userAgent.toLowerCase();
+$(document).ready(function() {
+
+	let ua = window.navigator.userAgent.toLowerCase();
 	if(ua.indexOf('iPhone') > 0) {
 		//iPhoneの時の処理
 		$("meta[name='viewport']").attr("content", "width=device-width,initial-scale=1");
@@ -16,16 +17,37 @@ $(function(){
 		//その他PCの処理
 		$("meta[name='viewport']").attr("content", "width=device-width,initial-scale=1");
 	}
-});
+
 
 
 // -----------------------------------
 // ハンバーガーメニュー
 // -----------------------------------	
-$(function(){
+
 	$('#hamburger').click(function(){
 		$('.g-nav').toggleClass('is-active');
 		$(this).toggleClass('is-active');
 		$('body').toggleClass('no-scroll');   // スクロールの禁止/解除
 	});
+
+    $(window).on('scroll', function() {
+        let footerOffset = $('footer').offset().top; // フッターの位置を取得
+        let windowHeight = $(window).height();       // 画面の高さ
+        let scrollTop = $(window).scrollTop();       // スクロール位置を取得
+        
+        if (scrollTop + windowHeight + 50 > footerOffset) {
+            $('#hamburger').fadeOut(); // footer50px上に差し掛かったら非表示
+        } else {
+            $('#hamburger').fadeIn();  // footerから離れたら再表示
+        }
+    });
+
+// -----------------------------------
+// TOPへ戻る
+// -----------------------------------	
+
+    $('#goTop').on('click', function(event) {
+        event.preventDefault(); // リンクのデフォルト動作を無効化
+        $('html, body').animate({ scrollTop: 0 }, 'slow'); // スムーズにスクロール
+    });
 });
